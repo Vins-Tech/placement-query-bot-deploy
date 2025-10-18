80804f8b0c4b70496a05b439217d8e35f1f174bd
Vins <vinaysvinays456@gmail.com>
2025-10-18 20:38:00 +0530

    testing

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ MAX_QUERIES = 25  # shared daily limit
 # -------------------------------
 # Cached initialization
 # -------------------------------
-@st.cache_resource(show_spinner=False)
 def init_vector_store():
     BASE_DIR = Path(__file__).parent
     folder = BASE_DIR / "resources/placement_texts"
@@ -38,7 +37,6 @@ if "initialized" not in st.session_state:
 # -------------------------------
 # Cached logo loading
 # -------------------------------
-@st.cache_data
 def load_logo_base64():
     logo_path = Path(__file__).parent / "resources/bnmit_logo.png"
     logo = Image.open(logo_path)
